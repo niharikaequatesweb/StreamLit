20f418175d858ec551fc0fac004b8243f3cc8b38
niharikaequatesweb <92165893+niharikaequatesweb@users.noreply.github.com>
2024-09-08 17:52:20 +0530

    commit

---
+++ app.py
@@ -0,0 +1,8 @@
+
+import streamlit as st
+st.header("Learning is funnnnnnnn!")
+
+
+code = '''def hello():
+    print("Hello, Streamlit!")'''
+st.code(code, language="python")
---
+++ stockmarketapp.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import yfinance as yf
+import datetime
+ticker_symbol = st.text_input("Enter the  stock", "AAPL")
+start_date = st.date_input("start_date", datetime.date(2019, 7, 6))
+end_date = st.date_input("end_date", datetime.date(2019, 7, 6))
+data=yf.download(ticker_symbol,start=start_date,end=end_date)
+
+st.write(data)
