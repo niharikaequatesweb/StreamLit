077fb445d5412f45a906ecfc77b8f40b6a518816
niharikaequatesweb <92165893+niharikaequatesweb@users.noreply.github.com>
2024-09-09 23:01:51 +0530

    commit to main

--- stockmarketapp.py
+++ stockmarketapp.py
@@ -1,9 +1,14 @@
 import streamlit as st
 import yfinance as yf
 import datetime
+
 ticker_symbol = st.text_input("Enter the  stock", "AAPL")
 start_date = st.date_input("start_date", datetime.date(2019, 7, 6))
-end_date = st.date_input("end_date", datetime.date(2019, 7, 6))
+end_date = st.date_input("end_date", datetime.date(2023, 1, 7))
+
 data=yf.download(ticker_symbol,start=start_date,end=end_date)
 
 st.write(data)
+
+st.line_chart(data["Close"])
+st.bar_chart(data["Volume"])
